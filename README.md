4b5ee542073eafd6c8091089caefbef9ff145fb4
Jacob Huynh <jacobshuynh2@gmail.com>
2023-10-03 01:05:52 -0400

    Added summary section on webpage and OpenAI API summarizer

---
+++ README.md
@@ -0,0 +1,8 @@
+This project takes in a .wav file input and outputs a transcribed version and summarized version of the file.
+
+In order to run the program, you must have python and flask installed.
+Before running, to allow the summary to be produced, you must input your OpenAI API Key at the top of the app.py file.
+
+Note that the transcription service can take a long time as it has to decipher the entire length of your .wav file.
+
+This project is still in development.
--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, redirect
+from dotenv import load_dotenv, dotenv_values   
+import os
 import speech_recognition as sr
+import openai
+
+load_dotenv()
+OpenAI_APIKey = os.getenv("OpenAI_APIKey")
+openai.api_key = OpenAI_APIKey
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
     transcript=""
+    summary=""
     if request.method == "POST":
         print("FORM DATA RECEIVED")
         if "file" not in request.files:
@@ -21,8 +29,13 @@ def index():
             with audioFile as source:
                 data = recognizer.record(source)
             transcript = recognizer.recognize_google(data, key=None)
+            presummary = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": "Summarize the following text:" + transcript}]
+            )
+            summary = (presummary['choices'][0]['message']['content']).strip("\n").strip()
             
-    return render_template("index.html",transcript=transcript)
+    return render_template("index.html",transcript=transcript,summary=summary)
 
 if __name__ == "__main__":
     app.run(debug=True, threaded = True)
